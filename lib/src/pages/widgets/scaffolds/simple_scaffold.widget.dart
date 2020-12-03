45179165b2e52b8f5567f045b22c11938411bfeb
Abelardo León González <abelardolg@gmail.com>
2020-12-03 20:45:51 +0100

    :sparkles: Add a body field to SimpleScaffoldWidget

--- lib/src/pages/widgets/scaffolds/simple_scaffold.widget.dart
+++ lib/src/pages/widgets/scaffolds/simple_scaffold.widget.dart
@@ -3,7 +3,10 @@ import 'package:flutter/material.dart';
 import 'package:contador/src/pages/widgets/appbars/simple_appbar.dart';
 class SimpleScaffoldWidget extends Scaffold {
 
-  SimpleScaffoldWidget (String appBarTitle, bool isTitleCentered) : super(appBar: SimpleAppbar(appBarTitle, isTitleCentered));
-
+  SimpleScaffoldWidget (String appBarTitle, bool isTitleCentered, Widget scaffoldBody) :
+        super(
+          appBar: SimpleAppbar(appBarTitle, isTitleCentered),
+          body: scaffoldBody
+        );
 }
 
