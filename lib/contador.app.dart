5e8ec37f0b1aec23981ed12ad169060161ae424a
Abelardo León González <abelardolg@gmail.com>
2020-12-03 19:43:57 +0100

    🎉 Initial Commit

---
+++ lib/contador.app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class ContadorApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Center(child: Text("Hola Mundo"))
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+import 'contador.app.dart';
+
+void main() {
+  runApp(new ContadorApp());
+}
